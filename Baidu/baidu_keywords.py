3f69ad1b8d9c06ee3208c11c56bab561c1b63324
lealexie <lealexie@outlook.com>
2018-02-25 14:53:06 +0800

    extract Info from baidu

---
+++ Baidu/baidu_keywords.py
@@ -0,0 +1,69 @@
+#According to keyword ......
+import requests
+import urllib
+import urllib2
+from bs4 import BeautifulSoup
+
+
+
+
+# headers
+User_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/54.0'
+headers = {'User-agent':User_agent}
+
+'''
+url = "http://www.baidu.com"
+request = urllib2.Request(url,headers=headers)
+response = urllib2.urlopen(request)
+print(response.read())
+'''
+
+baseURL = "https://www.baidu.com/s?"
+
+keywords_path = "/home/manyue/Project/Git/Spider/Baidu/keywords.txt" 
+link_path = "/home/manyue/Project/Git/Spider/Baidu/get_link.txt"
+text_path = "/home/manyue/Project/Git/Spider/Baidu/get_text.txt"
+
+with open(keywords_path,'r') as f:
+	for word in f:
+		data = {'ie':'utf-8','tn':'baiduurt','wd':word}
+		searchURL = baseURL + urllib.urlencode(data)
+
+		try:
+			request = urllib2.Request(searchURL,headers=headers)
+			response = urllib2.urlopen(request)
+			read_response = response.read()
+			soup = BeautifulSoup(read_response,"html.parser")
+			all_link = soup.find_all(attrs={'class':'result c-container '})
+
+			for link in all_link:
+				with open(link_path,'a') as f_w:
+					f_w.write(link.a['href'] + '\n')	
+
+		except Exception as e:
+			pass
+
+with open(link_path,'r') as f:
+	for link in f:
+		try:
+			request = urllib2.Request(link,headers=headers)
+			response = urllib2.urlopen(request)
+			read_response = response.read()
+			soup = BeautifulSoup(read_response,"html.parser")
+			find_text = soup.find_all('p')
+			for t in find_text:
+				content = t.string.strip()
+				print(content)
+			
+
+#				with open(text_path,'w') as w:
+#					w.write(content)
+				
+
+		
+
+		except Exception as e:
+			pass
+
+
+	
